311572031b366f54ddd61789eb7e6b346af0bab4
natasyaenspuri <if23.natasyapuri@mhs.ubpkarawang.ac.id>
2024-10-24 21:52:05 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Welcome </title>
+</head>
+<body>
+    <h1> first page </h1>
+    <form action="" method="Post">
+        <input type="email" name="email" id="email" placeholder="Email"/>
+        <input type="password" name="password" id="password" placeholder="password"/>
+        <button type="submit">Login</button>
+    </form>
+
+
+</body>
+</html>
